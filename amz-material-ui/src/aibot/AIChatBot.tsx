129c251238ce16dcd483baf34f2e850d5c8d0ccb
Danniel Nguyen <dannielnguyen289@gmail.com>
2025-10-04 02:03:38 +0900

    Chat bot example

--- amz-material-ui/src/aibot/AIChatBot.tsx
+++ amz-material-ui/src/aibot/AIChatBot.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Fragment, useEffect, useRef, useState } from 'react'
 import Box from '@mui/material/Box'
 import Slide from '@mui/material/Slide'
 import Stack from '@mui/material/Stack'
@@ -25,12 +25,121 @@ import FilePresentOutlinedIcon from '@mui/icons-material/FilePresentOutlined'
 import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined'
 import DataObjectOutlinedIcon from '@mui/icons-material/DataObjectOutlined'
 
+import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined'
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined'
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined'
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined'
+
 export type AIChatBotProps = {
   onClose: () => void
 }
 
+const listMessages = [
+  {
+    id: 1,
+    text: 'Hi!',
+    issuedBy: 'User',
+    issuedAt: new Date(),
+  },
+  {
+    id: 2,
+    element: <Typography variant="inherit">Hi there! How is it going?</Typography>,
+    issuedBy: 'AIBOT',
+    issuedAt: new Date(),
+  },
+  {
+    id: 3,
+    text: 'What could you help me?',
+    issuedBy: 'User',
+    issuedAt: new Date(),
+  },
+  {
+    id: 4,
+    element: (
+      <>
+        <Typography variant="inherit" sx={{ marginBottom: 1 }}>
+          I can help you with quite a lot of things, depending on what you need. For example:
+        </Typography>
+        <Typography variant="inherit" sx={{ marginBottom: 1 }}>
+          <strong>* Write & edit</strong>: emails, resumes, proposals, social media posts, blog articles, reports, etc.
+        </Typography>
+        <Typography variant="inherit" sx={{ marginBottom: 1 }}>
+          <strong>* Translate & explain</strong>: multiple languages, technical jargon, or simplify complex topics.
+        </Typography>
+        <Typography variant="inherit" sx={{ marginBottom: 1 }}>
+          <strong>* Research & summarize</strong>: gather info from the web, summarize documents, or compare options.
+        </Typography>
+        <Typography variant="inherit">
+          <strong>* Plan & structure</strong>: strategies for business, study, travel, projects, digital transformation,
+          etc.
+        </Typography>
+      </>
+    ),
+    issuedBy: 'AIBOT',
+    issuedAt: new Date(),
+  },
+]
+
 export default function AIChatBot(props: AIChatBotProps) {
+  const refListMessagesBox = useRef<HTMLElement>(null)
+
+  const [msg, setMsg] = useState('')
+  const [messages, setMessages] = useState(listMessages)
   const [isShowingAttachment, setIsShowingAttachment] = useState<boolean>(false)
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onChangeMsg = (event: any) => {
+    setMsg(event?.target?.value || '')
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onKeyDownMsg = (event: any) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault() // Prevent default Enter key behavior (e.g., new line)
+      sendMessage() // Call your form submission function
+    }
+  }
+
+  const sendMessage = () => {
+    if (msg.trim() !== '') {
+      const list = [...messages]
+      list.push({
+        id: list.length + 1,
+        text: msg.trim(),
+        issuedBy: 'User',
+        issuedAt: new Date(),
+      })
+      setMessages(list)
+      setMsg('')
+    }
+  }
+
+  useEffect(() => {
+    const last = messages[messages.length - 1]
+    if (last.issuedBy !== 'AIBOT') {
+      setTimeout(() => {
+        const list = [...messages]
+        list.push({
+          id: list.length + 1,
+          element: (
+            <>
+              <Typography>Sorry! I have trouble with server connection.</Typography>
+              <Typography>Please try again latter...</Typography>
+            </>
+          ),
+          issuedBy: 'AIBOT',
+          issuedAt: new Date(),
+        })
+        setMessages(list)
+      }, 1000)
+    }
+
+    // Scroll to bottom
+    if (refListMessagesBox.current) {
+      refListMessagesBox.current.scrollTop = refListMessagesBox.current.scrollHeight
+    }
+  }, [messages])
+
   return (
     <Box
       sx={{
@@ -91,7 +200,78 @@ export default function AIChatBot(props: AIChatBotProps) {
           </Stack>
         </Stack>
       </Box>
-      <Box>List</Box>
+      <Box
+        ref={refListMessagesBox}
+        sx={{
+          p: 2,
+          width: '100%',
+          height: `calc(100% - 40px -  54px)`,
+          maxWidth: '100%',
+          maxHeight: `calc(100% - 40px -  54px)`,
+          overflowX: 'hidden',
+          overflowY: 'auto',
+        }}
+      >
+        {messages.map((msg) => (
+          <Fragment key={msg.id}>
+            {msg.issuedBy === 'User' ? (
+              <Box
+                sx={{
+                  p: 2,
+                  pr: 0,
+                  display: 'flex',
+                  alignItems: 'start',
+                  justifyContent: 'end',
+                }}
+              >
+                <Box sx={{ py: 1, px: 3, borderRadius: 8, backgroundColor: blueGrey[50] }}>
+                  <Typography variant="inherit">{msg.text}</Typography>
+                </Box>
+              </Box>
+            ) : (
+              <Box
+                sx={{
+                  p: 2,
+                  pl: 0,
+                }}
+              >
+                {msg.element}
+                <Box>
+                  <Stack
+                    direction="row"
+                    spacing={1}
+                    sx={{
+                      justifyContent: 'start',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Stack>
+                      <IconButton tabIndex={3002} size="small" color="primary">
+                        <ContentCopyOutlinedIcon sx={{ fontSize: '1rem' }} />
+                      </IconButton>
+                    </Stack>
+                    <Stack>
+                      <IconButton tabIndex={3002} size="small" color="primary">
+                        <ThumbUpOutlinedIcon sx={{ fontSize: '1rem' }} />
+                      </IconButton>
+                    </Stack>
+                    <Stack>
+                      <IconButton tabIndex={3002} size="small" color="primary">
+                        <ThumbDownOutlinedIcon sx={{ fontSize: '1rem' }} />
+                      </IconButton>
+                    </Stack>
+                    <Stack>
+                      <IconButton tabIndex={3002} size="small" color="primary">
+                        <MoreHorizOutlinedIcon sx={{ fontSize: '1rem' }} />
+                      </IconButton>
+                    </Stack>
+                  </Stack>
+                </Box>
+              </Box>
+            )}
+          </Fragment>
+        ))}
+      </Box>
       <Box
         sx={{
           position: 'absolute',
@@ -145,12 +325,15 @@ export default function AIChatBot(props: AIChatBotProps) {
                 tabIndex={3000}
                 minRows={2}
                 maxRows={10}
+                value={msg}
+                onChange={onChangeMsg}
+                onKeyDown={onKeyDownMsg}
                 placeholder="Input your message here"
                 style={{ resize: 'vertical' }}
               />
             </Stack>
             <Stack sx={{ flexGrow: 0 }}>
-              <IconButton tabIndex={3002} size="small" color="primary">
+              <IconButton tabIndex={3002} size="small" color="primary" onClick={sendMessage}>
                 <SendOutlinedIcon />
               </IconButton>
             </Stack>
