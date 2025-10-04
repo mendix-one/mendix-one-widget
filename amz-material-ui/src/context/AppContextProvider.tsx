8a9125f7b6ffc032d4a5c3b6602404f1ea62b510
Danniel Nguyen <dannielnguyen289@gmail.com>
2025-10-04 15:48:35 +0900

    Install lib

--- amz-material-ui/src/context/AppContextProvider.tsx
+++ amz-material-ui/src/context/AppContextProvider.tsx
@@ -1,7 +1,14 @@
 import { type PropsWithChildren } from 'react'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+
 import type { AppContextData } from './AppContext'
 import AppContext from './AppContext'
 
 export default function MyContextProvider({ auth, children }: PropsWithChildren<AppContextData>) {
-  return <AppContext.Provider value={{ auth } as AppContextData}>{children}</AppContext.Provider>
+  return (
+    <AppContext.Provider value={{ auth } as AppContextData}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>{children}</LocalizationProvider>
+    </AppContext.Provider>
+  )
 }
--- amz-material-ui/src/pages/table/master-table/MasterTablePage.tsx
+++ amz-material-ui/src/pages/table/master-table/MasterTablePage.tsx
@@ -1,5 +1,11 @@
-import Box from '@mui/material/Box'
+import { useMemo } from 'react'
+
+import DataGenerator from '../DataGenerator'
+import MasterTableWidget from '../../../widgets/master-table/MasterTableWidget'
 
 export default function MasterTablePage() {
-  return <Box>Master Table</Box>
+  const data = useMemo(() => {
+    return DataGenerator.data()
+  }, [])
+  return <MasterTableWidget />
 }
