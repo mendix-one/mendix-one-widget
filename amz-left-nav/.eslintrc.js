a71c4d9e12fe8433b3caf3ad257714530a9757b6
Danniel Nguyen <dannielnguyen289@gmail.com>
2025-10-12 13:05:04 +0700

    Remove old sources

--- amz-left-nav/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-const base = require('@mendix/pluggable-widgets-tools/configs/eslint.ts.base.json')
-
-module.exports = {
-  ...base,
-  igno,
-}
--- amz-web-page/src/components/AmzWebPagePreview.tsx
+++
@@ -1,6 +0,0 @@
-export type AmzWebPagePreviewProps = {
-}
-
-export default function AmzWebPagePreview(_props: AmzWebPagePreviewProps) {
-  return <div className='widget-hello-world'>Hello world</div>
-}
